017ec8ee24d9e4f977865e2e1a95cd106a3471dd
NehyaRagavan <133482941+NehyaRagavan@users.noreply.github.com>
2024-04-03 12:27:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello! Welcome to CSE'));
+app.get('/', (req, res) => res.send('Hello! Welcome to CSE1"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
